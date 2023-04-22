63bcf5e5c580771e36076b9b41ff193f565c0aff
Maya Cranor <mcranor@olin.edu>
2023-04-22 18:07:33 -0400

    Finished view code.

--- src/view.h
+++ src/view.h
@@ -1,20 +1,27 @@
 #pragma once
 
 #include <stdlib.h>
-//#include "model.h"
 
 /**
- * Print a text representation of the uno hand.
+ * Print a text representation of the uno game.
  *
- * @param game_state the state of each hand
- * @param player the hand
+ * @param top_card the top card of the deck
+ * @param hand_size an array of hand sizes in order of player number
+ * @param players the length of hand_size which is the number of players
+ * @param player_turn an int corresponding to which players turn it is
  */
-void print_game_state(const game_state* game_state);
+void print_game(char top_card[], int hand_sizes[], size_t players, int player_turn);
 
 /**
- * Print a text representation of the uno hand.
+ * Print a text representation of the uno hand and if it's the players turn.
  *
- * @param game_state the state of each hand
- * @param player the hand
+ * @param hand a string that represent the player's hand.
+ * @param player_turn an int corresponding to which players turn it is
+ * @param player_id an int corresponding to which player this is
  */
-void print_player_hand(const game_state* game_state, int player);
+void print_hand(char hand[], int player_turn, int player_id);
+
+/**
+ * Print an error to let the player know to remove
+ */
+void print_error();
