b5e098776bf08f8a914a35ebbca372b88a62098a
An Grocki <angrocki02@gmail.com>
2023-05-03 17:50:57 -0400

    Complete documentation on model

--- src/model.c
+++ src/model.c
@@ -10,6 +10,24 @@ card* make_card(char col, size_t val) {
 
 void free_card(card* card_) { free(card_); }
 
+deck* make_deck(void) {
+  deck* new_deck = malloc(sizeof(deck));
+  new_deck->head = NULL;
+  new_deck->size = 0;
+  return new_deck;
+}
+
+void free_deck(deck* deck_) {
+  card* current = deck_->head;
+  card* next = NULL;
+  while (current != NULL) {
+    next = current->next;
+    free_card(current);
+    current = next;
+  }
+  free(deck_);
+}
+
 void append_card(deck* deck_, char col, size_t val) {
   card* new = make_card(col, val);
   if (deck_->head == NULL) {
@@ -22,6 +40,29 @@ void append_card(deck* deck_, char col, size_t val) {
     current->next = new;
   }
   deck_->size++;
+}
+
+deck* make_uno_deck(void) {
+  deck* UNO_deck = make_deck();
+  // zero colors 
+  append_card(UNO_deck, 'R', 0);
+  append_card(UNO_deck, 'B', 0);
+  append_card(UNO_deck, 'Y', 0);
+  append_card(UNO_deck, 'G', 0);
+  for (size_t i = 0; i < 2; i++) {
+    for (size_t j = 1; j < SPECIAL_13; j++) {
+      append_card(UNO_deck, 'R', j);
+      append_card(UNO_deck, 'G', j);
+      append_card(UNO_deck, 'B', j);
+      append_card(UNO_deck, 'Y', j);
+    }
+  }
+  // Wild Cards
+  for (size_t i = 0; i < 4; i++) {
+    append_card(UNO_deck, 'W', SPECIAL_13);
+    append_card(UNO_deck, 'W', SPECIAL_14);
+  }
+  return UNO_deck;
 }
 
 void move_card(card* card_, deck* old_deck, deck* new_deck) {
@@ -56,45 +97,15 @@ void move_card(card* card_, deck* old_deck, deck* new_deck) {
   new_deck->size++;
 }
 
-deck* make_deck(void) {
-  deck* new_deck = malloc(sizeof(deck));
-  new_deck->head = NULL;
-  new_deck->size = 0;
-  return new_deck;
-}
-
-void free_deck(deck* deck_) {
+card* get_card_index(deck* deck_, size_t index) {
+  if(index >= deck_->size){
+    return NULL;
+  }
   card* current = deck_->head;
-  card* next = NULL;
-  while (current != NULL) {
-    next = current->next;
-    free_card(current);
-    current = next;
+  for (size_t i = 0; i < index; i++) {
+    current = current->next;
   }
-  free(deck_);
-}
-
-deck* make_UNO_deck(void) {
-  deck* UNO_deck = make_deck();
-  // zero colors 
-  append_card(UNO_deck, 'R', 0);
-  append_card(UNO_deck, 'B', 0);
-  append_card(UNO_deck, 'Y', 0);
-  append_card(UNO_deck, 'G', 0);
-  for (size_t i = 0; i < 2; i++) {
-    for (size_t j = 1; j < SPECIAL_13; j++) {
-      append_card(UNO_deck, 'R', j);
-      append_card(UNO_deck, 'G', j);
-      append_card(UNO_deck, 'B', j);
-      append_card(UNO_deck, 'Y', j);
-    }
-  }
-  // Wild Cards
-  for (size_t i = 0; i < 4; i++) {
-    append_card(UNO_deck, 'W', SPECIAL_13);
-    append_card(UNO_deck, 'W', SPECIAL_14);
-  }
-  return UNO_deck;
+  return current;
 }
 
 void shuffle(deck* deck_) {
@@ -109,16 +120,23 @@ void shuffle(deck* deck_) {
   }
 }
 
-card* get_card_index(deck* deck_, size_t index) {
-  if(index < 0 || index >= deck_->size){
-    return NULL;
+int equal(deck* deck_1, deck* deck_2) {
+  // Lengths are not equal
+  if (deck_1->size != deck_2->size) {
+    return 0;
   }
-  card* current = deck_->head;
-  for (size_t i = 0; i < index; i++) {
-    current = current->next;
+  card* card_1 = deck_1->head;
+  card* card_2 = deck_2->head;
+  while (card_1 != NULL) {
+    if (card_1->value != card_2->value || card_1->color != card_2->color) {
+      return 0;
+    }
+    card_1 = card_1->next;
+    card_2 = card_2->next;
   }
-  return current;
+  return 1;
 }
+
 card* find_card(deck* deck_, char col, size_t val) {
   card* current = deck_->head;
   for (size_t i = 0; i < deck_->size; i++) {
@@ -128,13 +146,6 @@ card* find_card(deck* deck_, char col, size_t val) {
     current = current->next;
   }
   return NULL;
-}
-
-int check_draw(game_state* state) {
-  if (state->discard.size == 0) {
-    return 1;
-  }
-  return 0;
 }
 
 void append_deck(deck* original, deck* new) {
@@ -153,9 +164,20 @@ void append_deck(deck* original, deck* new) {
   original->size = 0;
 }
 
+int check_draw(game_state* state) {
+  if (state->draw.size > 1) {
+    return 1;
+  }
+  return 0;
+}
+
 void refill_draw(game_state* state) {
   shuffle(&(state->discard));
   append_deck(&(state->discard), &(state->draw));
+}
+
+void draw_card(game_state* state){
+  move_card(state->draw.head, &state->draw, &state->turn.hand);
 }
 
 void switch_main_card(game_state* state, char col, size_t val) {
@@ -202,34 +224,17 @@ void free_order(order* order_) {
   }
   free(order_);
 }
-
-void append_order(order* order_, player* player_) {
-  if (order_->head == NULL) {
-    order_->head = player_;
-  } else {
-    player* current = order_->head;
-    while (current->next != order_->head) {
-      current = current->next;
-    }
-    current->next = player_;
-    player_->next = order_->head;
-    player_->prev = current;
+int check_uno(game_state* state){
+  if(state->turn.hand.size == 1){
+    return 1;
   }
+  return 0; 
+}
+int check_win(game_state* state){
+  if(state->turn.hand.size == 0){
+    return 1; 
+  }
+  return 0; 
 }
 
-int equal(deck* deck_1, deck* deck_2) {
-  // Lengths are not equal
-  if (deck_1->size != deck_2->size) {
-    return 0;
-  }
-  card* card_1 = deck_1->head;
-  card* card_2 = deck_2->head;
-  while (card_1 != NULL) {
-    if (card_1->value != card_2->value || card_1->color != card_2->color) {
-      return 0;
-    }
-    card_1 = card_1->next;
-    card_2 = card_2->next;
-  }
-  return 1;
-}
+
